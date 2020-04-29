5721268680d4fc66f13877fb39e3841ab926d754
busungg <busungg@naver.com>
2020-04-29 20:29:44 +0900

    ReactRouter 구현
    1. ReactRouter 구현

---
+++ router-tutorial/src/App.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Route, Link, Switch } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
+import Profiles from './Profiles';
+import HistorySample from './HistorySample';
+
+function App() {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to="/">홈</Link>
+        </li>
+        <li>
+          <Link to="/about">소개</Link>
+        </li>
+        <li>
+          <Link to="/profiles">프로필</Link>
+        </li>
+        <li>
+          <Link to="/history">히스토리</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/" component={Home} exact={true}></Route>
+        <Route path={['/about', '/info']} component={About}></Route>
+        <Route path={'/profiles'} component={Profiles}></Route>
+        <Route path={'/history'} component={HistorySample}></Route>
+        <Route
+          render={({ location, history }) => {
+            return (
+              <div>
+                <h2>이 페이지는 존재하지 않습니다.</h2>
+                <p>{location.pathname}</p>
+                <div>
+                  <button
+                    onClick={() => {
+                      history.push('/');
+                    }}
+                  >
+                    홈으로
+                  </button>
+                </div>
+              </div>
+            );
+          }}
+        ></Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default App;
---
+++ 정리글/React 정리글/React Router.md
@@ -0,0 +1,14 @@
+# Reac Router
+
+## 1. BrowserRouter(리액트 라우터 라이브러리)
+BrowserRouter라는 컴포넌트는 HTML5의  History API를 사용하여 페이지를 새로고침하지 않고도 주소를 변경하고, 현재 주소에 관련된 정보를 props로 쉽게 조회하거나 사용할 수 있도록 해줍니다.
+
+- History API
+    1. DOM의 Window 객체는 history 객체를 통해 브라우저의 세션 기록에 접근할 수 있는 방법을 제공합니다.
+
+
+https://tylermcginnis.com/react-router-nested-routes/
+
+
+## 2. URL로 client-side rendering 웹 페이지를 호출 할 시 서버에 해당 정적 file이 없는데도 오류가 발생하지 않는 이유는?
+https://create-react-app.dev/docs/deployment/#serving-apps-with-client-side-routing
