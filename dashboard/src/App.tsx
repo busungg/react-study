d2b6ff8a27be244a225d60cf7b4fd43ddbd0f1c9
busungg <busungg@naver.com>
2020-05-07 21:47:35 +0900

    React BootStrap Tabs 확인
    1. React BootStrap Tabs 확인
    2. React BootStrap Tabs CSS 변경

---
+++ dashboard/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Tabs from './component/atom/Tabs';
+
+function App() {
+  return (
+    <div className="App">
+      <Tabs></Tabs>
+    </div>
+  );
+}
+
+export default App;
